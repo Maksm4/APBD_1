bfd49c00d09e81927f7ee3235fb2f0218f4e90cc
s27857 <s27857@pjwstk.edu.pl>
2024-03-18 10:33:01 +0100

    created calc average method

--- APBD_git/APBD_git/Program.cs
+++ APBD_git/APBD_git/Program.cs
@@ -1,6 +1,10 @@
 ﻿Console.WriteLine("Hello, World!");
 Person mark = new Person("mark",16,"31311313312");
 mark.SaySomething();
+int[] numbers = {1,6,1,5,16};
+Console.WriteLine(Person.CalcAverage(numbers));
+
+
 public class Person
 {
     private string name;
@@ -17,5 +21,16 @@ public Person(string name, int age, string pesel)
 public void SaySomething()
 {
     Console.WriteLine("my name is " + name);
+}
+
+public static double CalcAverage(int[] numbers)
+{
+    double sum = 0;
+    foreach (int  number in numbers)
+    {
+        sum += number;
+    }
+
+    return sum / numbers.Length;
 }
 }
