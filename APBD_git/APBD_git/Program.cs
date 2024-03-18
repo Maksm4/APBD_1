15423cbadf5dc76ffd48e89e5031b72d2649372a
s27857 <s27857@pjwstk.edu.pl>
2024-03-18 10:29:06 +0100

    modification 1

--- APBD_git/APBD_git/Program.cs
+++ APBD_git/APBD_git/Program.cs
@@ -1,3 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿Console.WriteLine("Hello, World!");
 
-Console.WriteLine("Hello, World!");
+public class Person
+{
+    private string name;
+    private int age;
+    private string pesel;
+
+public Person(string name, int age, string pesel)
+{
+    this.name = name;
+    this.age = age;
+    this.pesel = pesel;
+}
+}
